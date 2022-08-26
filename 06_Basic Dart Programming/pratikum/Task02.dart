5eb9451bb79bd21675a11b7a64031af921ee5295
Khaerul Latif <khaerullatif21@gmail.com>
2022-08-26 23:18:55 +0700

    input resume session06

---
+++ 06_Basic Dart Programming/pratikum/Task02.dart
@@ -0,0 +1,8 @@
+void main() {
+  //Task No.2
+  String classes = "flutter";
+  String firstName = "khaerul";
+  String lastName = "latif";
+  print('Task No.2');
+  print('$classes'+'_'+'$firstName-$lastName');
+}
